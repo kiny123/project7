977cd1b34bb124ee97416f62e4558e4d8d59d702
Kiny <89192734912nik@gmail.com>
2023-01-17 20:10:55 +0300

    init

---
+++ project7/project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by nikita on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
