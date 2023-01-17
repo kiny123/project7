16fecdf80ef4001e88ab0a853785a5d56ff5fdc9
Kiny <89192734912nik@gmail.com>
2023-01-17 22:16:22 +0300

    day 33

---
+++ project7/project7/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  project7
+//
+//  Created by nikita on 17.01.2023.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
